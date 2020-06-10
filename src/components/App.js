3f82aba0930f4370e60bef1531dc30d031c6124e
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-06-10 16:40:57 -0400

    added Unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import axios from 'axios'
 
 class App extends React.Component {
 
+
   onSearchSubmit(term){
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: 'Client-ID e4d40704ad373cba1cbd7572f1fa154067833acfc4510343ea2ccd23e34cd9f7'
+      }
+    });
   }
 
   render() {
