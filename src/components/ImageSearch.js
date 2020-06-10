9cb647f496243dc578a430c2830ec32c2f970514
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-06-10 19:32:45 -0400

    image search display working

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './ImageList.css'
+import ImageCard from './ImageCard'
 
 const ImageSearch = (props) => {
-    const images = props.images.map(image => { return <img src={image.urls.regular}/>});
+    const images = props.images.map((image)=> { 
+        return <ImageCard key={image.id} image={image}/> });
 
-    return <div>
+    return <div className= "image-list">
         {images}
     </div>
 }
